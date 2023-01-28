81188a58bb0139990ce968dadc5beed65d1dce94
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-28 14:17:31 +0100

    Add files via upload

---
+++ Cwiczenia 6 Zad 3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Cwiczenia_6_Zad_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "mars";
+            string tekst2 = "";
+
+            for (int i = tekst.Length - 1; i >= 0; i--)
+                tekst2 += tekst[i];
+
+            Console.WriteLine(tekst2);
+
+
+        }
+    }
+}
